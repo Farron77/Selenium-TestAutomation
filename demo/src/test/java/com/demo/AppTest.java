888c1a2417a1f459a5552f6ad38995664112c6f5
Farron77 <akosiiman@gmail.com>
2021-07-03 22:12:08 +0800

    Added test to check gmail

--- demo/src/test/java/com/demo/AppTest.java
+++ demo/src/test/java/com/demo/AppTest.java
@@ -3,8 +3,12 @@ package com.demo;
 
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
@@ -14,23 +18,49 @@ public class AppTest
 {
     public String baseUrl = "https://www.google.com.ph/";
     public WebDriver driver;
+
         
 
-    @BeforeTest 
+    @BeforeTest
     public void SetUp()
     {
-    System.setProperty("webdriver.chrome.driver","C:\\Users\\akosi\\Desktop\\DevFiles\\chromedriver.exe");
+    System.setProperty("webdriver.chrome.driver","C:\\Users\\akosi\\Documents\\VSCode\\TestAutomation\\MavenSample\\chromedriver.exe");
     driver = new ChromeDriver();
+    } 
+
+
+    /* 
+    System.setProperty("webdriver.chrome.driver","C:\\Users\\akosi\\Desktop\\DevFiles\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        C:\\Users\\akosi\\Documents\\VSCode\\TestAutomation\\
+        */ 
+        
+    @Test
+    public void SearchInGoogle() {
+        
+       
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get(baseUrl);
+        WebElement search = driver.findElement(By.name("q"));
+        search.sendKeys("Selenium will search a keyword here");
+        long id = Thread.currentThread().getId();
+        System.out.println("Before test-method. Thread id is: " + id);
     }
 
     @Test
-    public void SearchInGoogle() {
-       driver.get(baseUrl);
-       WebElement search = driver.findElement(By.name("q"));
-       search.sendKeys("Hi");
+    public void NavigateToGmail() {
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get(baseUrl);
+        WebElement gmail = driver.findElement(By.xpath("//a[contains(text(),'Gmail')]"));
+        gmail.click();
+        long id = Thread.currentThread().getId();
+        System.out.println("Before test-method. Thread id is: " + id);
+
     }
+
  
-    @AfterTest
+   @AfterTest
 
     public void quitChrome() {
         driver.close();
